2bf32211769066a09768e8b11a9dcdc75d684179
Helano Rangel do Prado <89919132+Lanooo@users.noreply.github.com>
2021-11-24 09:45:41 -0300

    Create exerc8.py

---
+++ listaexercpy6/exerc8.py
@@ -0,0 +1,12 @@
+m =  int(input("Informe a média: (0 a 10)"))
+f = int(input("Informe a frequência: (0 a 100)"))
+
+if f < 75:
+    print("Você foi reprovado")
+    
+elif (f >= 75) and (m < 7):
+    print("Você está de recuperação")
+    
+else:
+    ("Você foi aprovado")
+    
